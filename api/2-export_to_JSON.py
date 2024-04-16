4178941cd5bf38c435149ce691293ae180f12c9e
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-16 09:53:56 +0200

    export data to json file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" using this REST API in CSV format"""
+import json
+import requests
+from sys import argv
+
+
+API_URL = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    USER_ID = argv[1]
+
+    """ check user's informations """
+    user_response = requests.get(f"{API_URL}/users/{USER_ID}").json()
+
+    """ check user's to do list """
+    todo_response = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
+
+    """ export data in json file """
+    data = {
+        USER_ID: [
+            {
+                "task": task['title'],
+                "completed": task['compleeted'],
+                "username": user_response['username']
+            }
+            for task in todo_response
+        ]
+    }
+
+    """ write to json file """
+    with open(f"{USER_ID}.json", mode='w') as json_file:
+        json.dump(data, json_file)
+
+    print(f"Data as been exported to {USER_ID}.json")
