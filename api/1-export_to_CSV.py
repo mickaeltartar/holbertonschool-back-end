9a129ae468aa73a9b96e9c46cd4a95ceaf912a86
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-16 09:38:50 +0200

    write to csv file

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" using this REST API in CSV format"""
+
+import csv
+import requests
+from sys import argv
+
+
+API_URL = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    USER_ID = argv[1]
+
+    """ check user's informations """
+    user_response = requests.get(f"{API_URL}/users/{USER_ID}").json()
+
+    """ check user's to do list """
+    todo_response = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
+
+    """ write to csv file"""
+    with open(f"{USER_ID}.csv", mode='w') as csv_file:
+        writter = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+    for task in todo_response:
+        writter.writerow([
+            user_response['id'],
+            user_response['username'],
+            task['completed'],
+            task['title']
+        ])
+
+        print(f"Data as beenv exported to {USER_ID}.csv")
